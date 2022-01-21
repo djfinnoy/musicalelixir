42e623c29c25ca3cd471d44788793db0873c4fcc
Didrik Finnøy <djfinnoy@protonmail.com>
2022-01-21 14:00:52 +0100

    major scale

--- lib/musicalelixir.ex
+++ lib/musicalelixir.ex
@@ -1,38 +1,53 @@
 defmodule Musicalelixir do
-  def notewheel(note) do
-    wheel = [
-      :C, 
-      {:Csharp, :Dflat}, 
-      :D, 
-      {:Dsharp, :Eflat}, 
-      :E, 
-      :F, 
-      {:Fsharp, :Gflat}, 
-      :G, 
-      {:Gsharp, :Aflat},
-      :A,
-      {:Asharp, :Bflat},
-      :B
-    ]
+  @spec notewheel() :: list(binary)
+  def notewheel() do
+    ["C", "", "D", "", "E", "F", "", "G", "", "A", "", "B"]
+  end
 
-    cond do
-      is_integer(note) ->
-        Enum.at(wheel, note)
-      is_atom(note) ->
-        note_index(note, wheel, 0)
+  @spec notewheel(binary) :: integer
+  def notewheel(note) when is_binary(note) do
+    natural_index = get_index(String.at(note, 0), notewheel())
+    accidentals = String.slice(note, 1..-1)
+    accidental_adjustment = cond do
+      String.length(accidentals) == 0 -> 0
+      String.match?(accidentals, ~r/^\#{0,}$/) -> String.length(accidentals)
+      String.match?(accidentals, ~r/^b{0,}$/) -> String.length(accidentals) * -1
+      true -> {:error, "Invalid note: #{note}"}
+    end
+    rem(natural_index + accidental_adjustment + 12, 12)
+  end
+
+  @spec majorscale(binary) :: list(binary)
+  def majorscale(root) do
+    intervals = [2, 2, 1, 2, 2, 2, 1]
+    build_scale(intervals, [root])
+  end
+
+  @spec build_scale(list(integer), list(binary)) :: list(binary)
+  defp build_scale(intervals, result) do
+    if intervals == [] do result else
+      naturals = ["C", "D", "E", "F", "G", "A", "B"]
+      prev_note = List.last(result)
+      prev_natural = String.at(prev_note, 0)
+      next_natural = Enum.at(naturals, rem(get_index(prev_natural, naturals) + 1, 7))
+      prev_wheelpos = notewheel(prev_note)
+      next_wheelpos = rem(prev_wheelpos + hd(intervals), 12)
+      adjustment = rem(next_wheelpos - notewheel(next_natural), 12)
+      accidentals = cond do
+        adjustment == 0 -> ""
+        adjustment > 6 -> String.duplicate("b", 12 - adjustment)
+        adjustment < -6 -> String.duplicate("#", 12 + adjustment)
+        adjustment > 0 -> String.duplicate("#", adjustment)
+        adjustment < 0 -> String.duplicate("b", adjustment * -1)
+      end
+      build_scale(tl(intervals), result ++ [next_natural <> accidentals])
     end
   end
 
-  defp note_index(note, list, count) do
-    unless count > 11 do
-      case hd(list) do
-        {_, ^note} -> count
-        {^note, _} -> count
-        ^note      -> count
-        _ -> note_index(note, tl(list), count + 1)
-      end
-    else
-    {:error, "Invalid note: #{note}"}
-    end
+  @spec get_index(any, list) :: integer
+  defp get_index(x, xs) do
+    Enum.with_index(xs)
+    |> List.keyfind!(x, 0)
+    |> elem(1)
   end
 end
--- test/musicalelixir_test.exs
+++ test/musicalelixir_test.exs
@@ -1,8 +1,29 @@
 defmodule MusicalelixirTest do
   use ExUnit.Case
   doctest Musicalelixir
+  import Musicalelixir
 
-  test "greets the world" do
-    assert Musicalelixir.hello() == :world
+  test "majorscales" do
+    assert majorscale("Cb") == ["Cb", "Db", "Eb", "Fb", "Gb", "Ab", "Bb", "Cb"]
+    assert majorscale("C")  == ["C", "D", "E", "F", "G", "A", "B", "C"]
+    assert majorscale("C#") == ["C#", "D#", "E#", "F#", "G#", "A#", "B#", "C#"]
+    assert majorscale("Db") == ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C", "Db"]
+    assert majorscale("D")  == ["D", "E", "F#", "G", "A", "B", "C#", "D"]
+    assert majorscale("D#") == ["D#", "E#", "F##", "G#", "A#", "B#", "C##", "D#"]
+    assert majorscale("Eb") == ["Eb", "F", "G", "Ab", "Bb", "C", "D", "Eb"]
+    assert majorscale("E")  == ["E", "F#", "G#", "A", "B", "C#", "D#", "E"]
+    assert majorscale("E#") == ["E#", "F##", "G##", "A#", "B#", "C##", "D##", "E#"]
+    assert majorscale("Fb") == ["Fb", "Gb", "Ab", "Bbb", "Cb", "Db", "Eb", "Fb"]
+    assert majorscale("F")  == ["F", "G", "A", "Bb", "C", "D", "E", "F"]
+    assert majorscale("F#") == ["F#", "G#", "A#", "B", "C#", "D#", "E#", "F#"]
+    assert majorscale("Gb") == ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F", "Gb"]
+    assert majorscale("G")  == ["G", "A", "B", "C", "D", "E", "F#", "G"]
+    assert majorscale("G#") == ["G#", "A#", "B#", "C#", "D#", "E#", "F##", "G#"]
+    assert majorscale("Ab") == ["Ab", "Bb", "C", "Db", "Eb", "F", "G", "Ab"]
+    assert majorscale("A")  == ["A", "B", "C#", "D", "E", "F#", "G#", "A"]
+    assert majorscale("A#") == ["A#", "B#", "C##", "D#", "E#", "F##", "G##", "A#"]
+    assert majorscale("Bb") == ["Bb", "C", "D", "Eb", "F", "G", "A", "Bb"]
+    assert majorscale("B")  == ["B", "C#", "D#", "E", "F#", "G#", "A#", "B"]
+    assert majorscale("B#") == ["B#", "C##", "D##", "E#", "F##", "G##", "A##", "B#"]
   end
 end
